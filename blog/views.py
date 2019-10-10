f33436bb8dcc85d01a7485a5d2368830d2a566a2
Karolin Flach <karolin.flach@gmail.com>
2019-10-10 06:31:14 +0200

    save new posts as drafts and add publish and delete options

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,7 @@ def post_list(request):
 
     posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by(
-        'published_date'
-    )
+        'published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
@@ -26,7 +25,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
 
             return redirect('post_detail', pk=post.pk)
@@ -44,7 +42,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
 
             return redirect('post_detail', pk=post.pk)
@@ -54,12 +51,26 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+def post_draft_list(request):
+    posts = Post.objects.filter(
+        published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+
 def about_page(request):
     return render(request, 'about.html')
-
-
-def contact_page(request):
-    return render(request, 'contact.html')
 
 
 def homepage(request):
